2e8533f4d1d686247f24f19b3fbabc8aa5566378
hojeong <parkhj062@gmail.com>
2026-01-21 16:05:36 +0900

    [77. 빈 후처리기 - 적용]

---
+++ src/main/java/hello/proxy/config/v4_postprocessor/postprocessor/PackageLogTracePostProcessor.java
@@ -0,0 +1,35 @@
+package hello.proxy.config.v4_postprocessor.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+@Slf4j
+public class PackageLogTracePostProcessor implements BeanPostProcessor {
+    private final String basePackage;
+    private final Advisor advisor;
+
+    public PackageLogTracePostProcessor(String basePackage, Advisor advisor) {
+        this.basePackage = basePackage;
+        this.advisor = advisor;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        log.info("beanName={}, bean={}", beanName, bean);
+        Class<?> targetClass = AopUtils.getTargetClass(bean);
+        if (!targetClass.getPackageName().startsWith(basePackage)) {
+            return bean;
+        }
+
+        ProxyFactory proxyFactory = new ProxyFactory(bean);
+        proxyFactory.addAdvisor(advisor);
+
+        Object proxy = proxyFactory.getProxy();
+        log.info("creation: target={}, proxy={}", proxy.getClass(), bean.getClass());
+        return proxy;
+    }
+}
